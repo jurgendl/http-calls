0b7fc20fc86f846c96d4f036f07fc7a697eb18e4
jurgen de landsheer <jurgen.delandsheer@ugent.be>
2025-02-28 15:09:03 +0100

    http calls

--- _api/routes/users.js
+++ _api/routes/users.js
@@ -24,41 +24,51 @@ router.get('/', (req, res) => {
 
 // Adding users to our mock database
 router.post('/', (req, res) => {
+	res.setHeader('Content-Type', 'application/json');
 	let user = req.body;
 	const id = uuidv4();
 	user = { ...user, id };
 	users.push(user);
-	res.setHeader('Content-Type', 'application/json');
-	res.send(`{"action":"created","user":${JSON.stringify(user)}}`);
+	res.send(`{"action":"create","user":${JSON.stringify(user)}}`);
 })
 
 // get a particular user
 router.get('/:id', (req, res) => {
+	res.setHeader('Content-Type', 'application/json');
 	const { id } = req.params;
 	const foundUser = users.find((user) => user.id === id);
-	res.setHeader('Content-Type', 'application/json');
 	res.send(foundUser)
 });
 
 // delete the user from the database
 router.delete('/:id', (req, res) => {
-	const { id } = req.params;
-	const foundUser = users.find((user) => user.id !== id);
 	res.setHeader('Content-Type', 'application/json');
-	res.send(`{"action":"deleted","user":${JSON.stringify(foundUser)}}`);
+	const { id } = req.params;
+	const foundUser = users.find((user) => user.id === id);
+	if(!foundUser) {
+		res.statusCode = 404
+		res.send(`{"action":"delete","id":"${id}","error":"404"}`);
+		return;
+	}
+	users.splice(users.indexOf(foundUser), 1);
+	res.send(`{"action":"delete","id":"${id}","user":${JSON.stringify(foundUser)}}`);
 });
 
 // Make a PATCH request to the database
 router.patch('/:id', (req, res) => {
+	res.setHeader('Content-Type', 'application/json');
 	const { id } = req.params;
 	const { first_name, last_name, email } = req.body;
 	const foundUser = users.find((user) => user.id === id);
-	if(!foundUser) 
+	if(!foundUser) {
+		res.statusCode = 404
+		res.send(`{"action":"patch","id":"${id}","error":"404"}`);
+		return;
+	}
 	if (first_name) foundUser.first_name = first_name;
 	if (last_name) foundUser.last_name = last_name;
 	if (email) foundUser.email = email;
-	res.setHeader('Content-Type', 'application/json');
-	res.send(foundUser);
+	res.send(`{"action":"patch","user":${JSON.stringify(foundUser)}}`);
 });
 
 export default router
