5547c58b65fef73ec7ec00aa30adbba2204c3c37
jurgen de landsheer <jurgen.delandsheer@ugent.be>
2025-02-28 12:46:26 +0100

    http calls

--- httpie/httpie-calls.bat
+++ httpie/httpie-calls.bat
@@ -1,7 +1,7 @@
 ::https://httpie.io/docs/cli
 @cls
-http -v GET https://httpbin.org/get?isGood=true&isBad=false
-http -v DELETE https://httpbin.org/delete
-http -v POST https://httpbin.org/post name="john deer" permissions:=[2000,3000,4000]
-http -v PUT https://httpbin.org/put --form "name=john%20deer&permissions=2000&permissions=3000&permissions=4000"
+http -v GET "https://httpbin.org/get?isGood=true&isBad=false"
+http -v DELETE "https://httpbin.org/delete"
+http -v POST "https://httpbin.org/post" name="john deer" permissions:=[2000,3000,4000]
+http -v PUT "https://httpbin.org/put" --form "name=john%20deer&permissions=2000&permissions=3000&permissions=4000"
 @pause
