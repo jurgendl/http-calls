f4739abbe843991732b648cbb46170c672b8fb8c
jurgen de landsheer <jurgen.delandsheer@ugent.be>
2025-02-28 10:48:03 +0100

    http calls

--- curlie/curlie-calls.bat
+++ curlie/curlie-calls.bat
@@ -1,6 +1,7 @@
-cls
-curlie -X GET "https://httpbin.org/get?isGood=true&isBad=false"
-curlie -X DELETE "https://httpbin.org/delete"
-curlie -X POST "https://httpbin.org/post" --data "{""name"": ""john"", ""permissions"": [2000,3000,4000]}"
-curlie -X PUT "https://httpbin.org/put" --data "name=john&permissions=2000&permissions=3000&permissions=4000" -H "Content-Type: application/x-www-form-urlencoded"
-pause
+::https://rs.github.io/curlie/
+@cls
+curlie -v GET "https://httpbin.org/get?isGood=true&isBad=false"
+curlie -v DELETE "https://httpbin.org/delete"
+curlie -v POST "https://httpbin.org/post" --json "{ \"name\": \"john\", \"permissions\": [2000,3000,4000] }"
+curlie -v PUT "https://httpbin.org/put" --data "name=john&permissions=2000&permissions=3000&permissions=4000" -H "Content-Type: application/x-www-form-urlencoded"
+@pause
