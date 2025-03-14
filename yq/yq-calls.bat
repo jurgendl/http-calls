cef14958faedbba9aaa0342099af457c2812ab45
jdlandsh <jurgen.delandsheer@ugent.be>
2025-03-14 16:14:56 +0100

    yq

---
+++ yq/yq-calls.bat
@@ -0,0 +1,7 @@
+:: https://mikefarah.gitbook.io/yq
+:: https://spacelift.io/blog/yaml
+:: https://docs.ansible.com/ansible/latest/reference_appendices/YAMLSyntax.html
+@cls
+yq ".foods" yaml.yml
+yq ".education" yaml.yml
+@pause
