f66e518b821e6ac5a196ba01f9f38fe81622d1c9
jdlandsh <jurgen.delandsheer@ugent.be>
2025-10-29 09:45:43 +0100

    python installed via pyenv

--- httpie/httpie-calls.bat
+++ httpie/httpie-calls.bat
@@ -1,15 +1,15 @@
-::call https://call httpie.io/docs/cli
+::call https://httpie.io/docs/cli
 @cls
 call http --help
-call http --verbose GET "call https://call httpbin.org/get?isGood=true&isBad=false"
-call http --verbose DELETE "call https://call httpbin.org/delete"
-call http --verbose POST "call https://call httpbin.org/post" name="john deer" permissions:=[2000,3000,4000]
-call http --verbose PUT "call https://call httpbin.org/put" --form name="john deer" permissions=2000 permissions=3000 permissions=4000
-call http --verbose GET "call https://call httpbin.org/hidden-basic-auth/u/p" "Authorization: Basic dTpw"
-call http --verbose GET "call https://call httpbin.org/image/svg" "accept: image/svg+xml"
-call http --verbose GET "call https://call httpbin.org/drip?duration=2&numbytes=10&code=200&delay=2" "accept: application/octet-stream" --chunked --stream 
-call http --verbose GET "call https://call httpbin.org/bearer" "accept: application/json" "Authorization: Bearer bearer"
-call http --verbose --download GET "call https://call httpbin.org/gzip" --output "gzip.json"
-call http --verbose --download GET "call https://github.com/call httpie/cli/archive/master.tar.gz" --output "master.tar.gz"
-call http --verbose --download GET "call https://call httpbin.org/stream-bytes/1048576" --output "1048576.bin"
+call http --verbose GET "https://httpbin.org/get?isGood=true&isBad=false"
+call http --verbose DELETE "https://httpbin.org/delete"
+call http --verbose POST "https://httpbin.org/post" name="john deer" permissions:=[2000,3000,4000]
+call http --verbose PUT "https://httpbin.org/put" --form name="john deer" permissions=2000 permissions=3000 permissions=4000
+call http --verbose GET "https://httpbin.org/hidden-basic-auth/u/p" "Authorization: Basic dTpw"
+call http --verbose GET "https://httpbin.org/image/svg" "accept: image/svg+xml"
+call http --verbose GET "https://httpbin.org/drip?duration=2&numbytes=10&code=200&delay=2" "accept: application/octet-stream" --chunked --stream 
+call http --verbose GET "https://httpbin.org/bearer" "accept: application/json" "Authorization: Bearer bearer"
+call http --verbose --download GET "https://httpbin.org/gzip" --output "gzip.json"
+call http --verbose --download GET "https://github.com/call httpie/cli/archive/master.tar.gz" --output "master.tar.gz"
+call http --verbose --download GET "https://httpbin.org/stream-bytes/1048576" --output "1048576.bin"
 @pause
