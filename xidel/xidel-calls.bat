5c32627505856058a19c7e0e300ccd24e8256091
jdlandsh <jurgen.delandsheer@ugent.be>
2025-11-24 16:18:02 +0100

    commandline utils

--- xidel/xidel-calls.bat
+++ xidel/xidel-calls.bat
@@ -1,4 +1,5 @@
 ::https://www.videlibri.de/xidel.html
+::https://github.com/benibela/xidel
 @cls
 xidel --help
 curl --silent -X GET "https://httpbin.org/html" | xidel - --silent --extract "//html//body//h1//text()"
