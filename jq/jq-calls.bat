ab7354ade8982a0a334970b507d77424e3db1017
jurgendl <jurgen.de.landsheer@gmail.com>
2026-01-07 23:57:24 +0100

    python

--- jq/jq-calls.bat
+++ jq/jq-calls.bat
@@ -1,4 +1,5 @@
 ::https://jqlang.org/
+::https://github.com/itchyny/gojq
 @cls
 jq --help
 curl --silent -X GET "https://httpbin.org/get" | jq ".headers.\"User-Agent\"" --compact-output --raw-output
